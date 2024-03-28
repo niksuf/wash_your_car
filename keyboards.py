09ecee7a7f1335d08c307134d819fc2ed5bc3fd1
niksuf <niksuf1@yandex.ru>
2024-03-29 02:21:13 +0300

    added restart func and use old location func

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import emoji
 
+# 1st keyboard
 send_position = KeyboardButton(text=emoji.emojize('Отправить геопозицию :round_pushpin:'), request_location=True)
 start_keyboard = ReplyKeyboardMarkup(keyboard=[[send_position]], resize_keyboard=True)
+
+# 2nd keyboard
+use_old_position = KeyboardButton(text=emoji.emojize('Использовать старую геолокацию'))
+second_keyboard = ReplyKeyboardMarkup(keyboard=[[send_position], [use_old_position]], resize_keyboard=True)
