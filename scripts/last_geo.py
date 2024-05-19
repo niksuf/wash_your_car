3bd3ec0cda5ad032df05a89980f23ff294f774bb
niksuf <niksuf1@yandex.ru>
2024-05-20 01:31:13 +0300

    fixed context manager

--- scripts/last_geo.py
+++ scripts/last_geo.py
@@ -1,11 +1,12 @@
 import psycopg2
+from contextlib import closing
 
 
 def connect_to_db(dbname, user, password, host):
     try:
         conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
         cur = conn.cursor()
-        return conn, cur
+        return closing(conn), closing(cur)
     except psycopg2.Error as e:
         print(f"Error connecting to the database: {e}")
         return None, None
@@ -50,17 +51,18 @@ def main():
                        'newuser',
                        'password',
                        'localhost') as (conn, cur):
-        insert_last_geo(conn,
-                        cur,
-                        '2024-05-20',
-                        'TestUser',
-                        55.7558,
-                        37.6173,
-                        '14:00:00')
-        get_last_geo(cur)
+        if conn and cur:
+            insert_last_geo(conn,
+                            cur,
+                            '2024-05-20',
+                            'TestUser',
+                            55.7558,
+                            37.6173,
+                            '14:00:00')
+            get_last_geo(cur)
 
-        update_last_geo(conn, cur, 'UpdatedUser', 'TestUser')
-        get_last_geo(cur)
+            update_last_geo(conn, cur, 'UpdatedUser', 'TestUser')
+            get_last_geo(cur)
 
 
 if __name__ == '__main__':
